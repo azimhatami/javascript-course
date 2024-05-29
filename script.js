ab4c0521e912e7a8d42cb3509b6bd010a95c7734
Azim Hatami <azimhatami4@gmail.com>
2024-05-29 20:20:36 +0330

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Variable
+var c = 4;
+// console.log(var1);
+
+c = 'I am string now';
+// console.log(var1);
+
+// console.log(3<2<1)
+
+// var a = 1+'2';
+// var a = 3+ 4*5;
+var a = 1, b = 2, c = 3;
+console.log(a=b=c);
