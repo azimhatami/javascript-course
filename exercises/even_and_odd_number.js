5feddb4d163577d3d85527e57771ea0c56ec3c21
Azim Hatami <azimhatami4@gmail.com>
2024-05-30 19:30:56 +0330

    Solve exercise with students

---
+++ exercises/even_and_odd_number.js
@@ -0,0 +1,14 @@
+// Exercise
+
+function check_even_or_odd () {
+  for (var number = 0; number <= 10; number++) {
+    if (number % 2 == 0) {
+      console.log(`Even -> ${number}`);
+    } else {
+      console.log(`Odd -> ${number}`);
+    };
+  };
+};
+
+check_even_or_odd()
+
