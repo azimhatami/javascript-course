7311e51be6429b20e527cc683008df7596bcd058
Azim Hatami <azimhatami4@gmail.com>
2024-05-30 19:15:47 +0330

    Rename the file

---
+++ variable.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Variable
+var c = 4;
+console.log(c);
+
+// Data type
+var var1 = 3;
+console.log(typeof var1);
+
+var var2 = 3.2;
+console.log(typeof var2);
+
+var var3 = 'Text';
+console.log(typeof var3);
+
+var var4 = null;
+console.log(typeof var4);
+
+var var5 = undefined;
+console.log(typeof var51);
+
+var var6 = [1, 3, 'a', true];
+console.log(typeof var6);
+
+var var7 = {
+  name: 'Azim',
+  age: 20
+};
+console.log(typeof var7);
+
+// Operators
+var numbers1 = 1+'2';
+console.log(numbers1);
+
+var numbers2 = 3+ 4*5;
+console.log(numbers2);
+
+// Strict mode
+num = 5;
+console.log(num); // get an error
