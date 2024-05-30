242b71580744719098b23903749f4775084f710c
Azim Hatami <azimhatami4@gmail.com>
2024-05-30 19:16:31 +0330

    Learn to if statement

---
+++ if_statement.js
@@ -0,0 +1,12 @@
+// if statement
+
+var condition = false;
+var anotherCondition = false;
+
+if (condition) {
+  console.log('Executed!')
+} else if (anotherCondition) {
+  console.log('Still Executed!')
+} else {
+  console.log('Not Executed!')
+};
