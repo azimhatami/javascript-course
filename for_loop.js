c03d48ef052f2df13a394a17a08c8d1d464c939e
azimhatami <azimhatami4@gmail.com>
2024-06-02 00:20:40 +0330

    For loop on array

--- for_loop.js
+++ for_loop.js
@@ -26,5 +26,25 @@ for (var i = 1; i <= 10; i++) {
   for (var j = 1; j <= 10; j++) {
     stars += '*';
   };
-  console.log(stars);
+  // console.log(stars);
+};
+
+
+// For loop break and continue
+for (var i = 0; i < 5; i++) {
+  for (var j = 0; j < 2; j++) {
+    if (i == 1) {
+      // continue;
+      break;
+    };
+    // console.log('Inside inner loop, j =' + j);
+  };
+  // console.log(i);
+};
+
+// For loop On array
+var array = [1, 2, 3]
+
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
 };
