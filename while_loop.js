12940259f65116e3ec349c839e6112e2bdb5ff97
azimhatami <azimhatami4@gmail.com>
2024-06-02 00:22:04 +0330

    Learn while loop

---
+++ while_loop.js
@@ -0,0 +1,25 @@
+// Control structures while loop
+
+var number = 5;
+
+while (number < 7) {
+  // console.log(number);
+  number++;
+};
+
+
+var condition = true;
+var i = 2;
+
+// while (condition) {
+  // if (i == 3) {
+    // condition = false;
+  // };
+  // console.log(i);
+  // i++
+// };
+
+do {
+  console.log('Executed')
+}
+while (condition);
