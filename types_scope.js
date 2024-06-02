767614ef20f9d1e69b38605ddec75ff6eb9bb22e
azimhatami <azimhatami4@gmail.com>
2024-06-02 18:48:00 +0330

    Learn types & scope

---
+++ types_scope.js
@@ -0,0 +1,29 @@
+// Types & Scope
+
+// var aNumber = 5;
+// console.log(aNumber); // -> 5
+
+// var anotherNumber = aNumber;
+// console.log(anotherNumber); // -> 5
+
+// var aNumber = 12;
+// console.log(aNumber);
+// console.log(anotherNumber);
+
+
+// var array = [1, 2, 3];
+// var anotherArray = array;
+
+// console.log(array); // -> [1, 2, 3]
+// console.log(anotherArray); // -> [1, 2, 3]
+
+// array.push(4);
+
+// console.log(array); // -> [1, 2, 3, 4]
+// console.log(anotherArray); // -> [1, 2, 3, 4]
+
+// var array = ['a', 'b', 'c'];
+
+// console.log(array); // -> ['a', 'b', 'c']
+// console.log(anotherArray); // -> [1, 2, 3, 4]
+
