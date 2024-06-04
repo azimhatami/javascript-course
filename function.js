5853d4725a5c0e4d2b4cc923d6ffd6800b2e949a
azimhatami <azimhatami4@gmail.com>
2024-06-05 01:48:19 +0330

    Learn arrow function

--- function.js
+++ function.js
@@ -22,3 +22,19 @@ function calc(number1, number2) {
 
 var calculator = calc;
 console.log(calculator(10, 3));
+
+// Arrow function
+const say_hello = () => {
+  console.log('Good Morning');
+}
+
+say_hello();
+
+
+const add = (a, b) => a+b;
+
+console.log(add(3, 2));
+
+let numbers = [1, 2, 4, 7, 8, 5];
+
+numbers.forEach(number => console.log(number));
