e6939cc520e5fa87cf55c4d0e39674a8042d6498
azimhatami <azimhatami4@gmail.com>
2024-06-05 02:09:08 +0330

    Learn built in objects

---
+++ built_in_objects.js
@@ -0,0 +1,30 @@
+ console.log(Number);
+// console.log(Number.EPSILON);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MAX_VALUE * 10);
+// console.log(Number.isFinite(Number.MAX_VALUE * 10));
+
+console.log(Math);
+// console.log(Math.E);
+// console.log(Math.PI);
+
+function circle_primeter(d) {
+  console.log(d * Math.PI);
+};
+
+circle_primeter(5);
+
+
+// console.log(Math.sqrt(9));
+// console.log(9 ** 0.5);
+
+// console.log(Math.pow(10, 2));
+// console.log(10 ** 2);
+
+// console.log(Math.random());
+// console.log(Math.round(5.49));
+// console.log(Math.round(Math.random() * 5));
+
+
+console.log(Date);
