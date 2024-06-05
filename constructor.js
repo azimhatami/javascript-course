626d45718085a3f51699f7f890848482567132e8
azimhatami <azimhatami4@gmail.com>
2024-06-06 00:30:12 +0330

    Learn prototype

--- constructor.js
+++ constructor.js
@@ -10,19 +10,28 @@ function Person(firstname, lastname, age) {
   };
 };
 
+Person.prototype.eye_color = 'black'
+
+console.log(Person());
 
 const ali = new Person('Ali', 'Hatami', 15);
+ali.city = 'Tehran';
 console.log(ali);
 console.log(ali.firstname);
 console.log(ali.lastname);
 console.log(ali.full_name());
 console.log(ali.age);
+console.log(ali.city);
+console.log(ali.eye_color);
+
 console.log('-----------------');
 
 const mmd = new Person('Mmd', 'Hatami', 10);
+mmd.city = 'Ney'
 console.log(mmd);
 console.log(mmd.firstname);
 console.log(mmd.lastname);
 console.log(mmd.full_name());
 console.log(mmd.age);
-
+console.log(mmd.city);
+console.log(ali.eye_color);
